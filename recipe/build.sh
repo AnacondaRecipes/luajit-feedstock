c728ba894500f6d813520d6390dd8d05ab4b910c
Viacheslav Ch. <vchychyrko@anaconda.com>
2026-04-07 23:02:41 +0300

    fix review

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,8 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-make amalg PREFIX=${PREFIX} XCFLAGS=-DLUAJIT_ENABLE_GC64
-make install PREFIX=${PREFIX} XCFLAGS=-DLUAJIT_ENABLE_GC64
+make amalg PREFIX=${PREFIX} XCFLAGS=-DLUAJIT_ENABLE_GC64 CC=${CC}
+make install PREFIX=${PREFIX} XCFLAGS=-DLUAJIT_ENABLE_GC64 CC=${CC}
 
 ln -sf ${PREFIX}/lib/libluajit-5.1${SHLIB_EXT} ${PREFIX}/lib/libluajit${SHLIB_EXT}
 
